468dedd29354d5c203c082c4c45674e067be4d52
Evgeny Tolkachev <46066387+etolk@users.noreply.github.com>
2023-12-01 15:24:22 +0100

    Create cookie-banner.js

---
+++ js/cookie-banner.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  if (getCookie("cookieConsent") === "accepted") {
+    document.getElementById('cookieBanner').style.display = 'none';
+  } else {
+    document.getElementById('cookieBanner').style.display = 'block';
+  }
+};
+
+function acceptCookies() {
+  document.cookie = "cookieConsent=accepted; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+  window.location.reload();
+}
+
+function declineCookies() {
+  document.getElementById('cookieBanner').style.display = 'none';
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
